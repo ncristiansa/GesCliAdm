0c90b6fee6379a44e5daf6d6a9a08e92f1cc4d2c
ncristiansa <ncristiansalinasandia@gmail.com>
2019-04-08 20:17:45 +0200

    change

--- resources/views/clients/clientes.blade.php
+++ resources/views/clients/clientes.blade.php
@@ -25,13 +25,10 @@
     <script>
         
         var clientes = {!! json_encode($clientes->toArray(), JSON_HEX_TAG) !!} ;
-
-        console.log(clientes)
-
         CreateTable("#ClientsTable",clientes.data,undefined);
-
         createFilter('#ClientsTable table thead',"/","clientes","table");
-        
+       
+
        $('.clickable').each(function(){
             $(this).attr("data-href","/clients/"+$(this).attr("id"));
        })
